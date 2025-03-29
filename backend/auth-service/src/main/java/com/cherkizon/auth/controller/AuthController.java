a8dc4113e2543685d6126117324bec57021ae9c8
Goodwin999 <Vyacheslav.tsitsar@mail.ru>
2025-03-29 07:53:40 +0300

    feat(auth): created repository methods; created exceptions; created interface implementation; created DTO; created controller class;

--- backend/auth-service/src/main/java/com/cherkizon/auth/controller/AuthController.java
+++ backend/auth-service/src/main/java/com/cherkizon/auth/controller/AuthController.java
@@ -1,4 +1,50 @@
 package com.cherkizon.auth.controller;
 
+import com.cherkizon.auth.dto.LoginRequest;
+import com.cherkizon.auth.dto.RegisterRequest;
+import com.cherkizon.auth.dto.response.JwtResponse;
+import com.cherkizon.auth.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(value = "http://localhost:3000")
+@RestController
+@RequestMapping("/api/auth")
+@Tag(name = "Аутентификация", description = "API для регистрации и входа")
 public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @Operation(summary = "Регистрация пользователя",
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Пользователь создан"),
+                    @ApiResponse(responseCode = "400", description = "Невалидные данные"),
+                    @ApiResponse(responseCode = "409", description = "Пользователь уже существует")
+            })
+    @PostMapping("/register")
+    public Long register(@Valid @RequestBody RegisterRequest request) {
+        return authService.register(request);
+    }
+
+    @Operation(summary = "Аутентификация пользователя",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Успешная аутентификация"),
+                    @ApiResponse(responseCode = "400", description = "Невалидные данные"),
+                    @ApiResponse(responseCode = "401", description = "Неверные учетные данные")
+            })
+    @PostMapping("/login")
+    public JwtResponse login(@Valid @RequestBody LoginRequest request) {
+        return authService.login(request);
+    }
 }
