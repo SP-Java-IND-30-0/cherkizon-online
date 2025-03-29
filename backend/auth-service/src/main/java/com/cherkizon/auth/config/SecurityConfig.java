12b91b3166782e8e51d5c559981d3517c740f362
Goodwin999 <Vyacheslav.tsitsar@mail.ru>
2025-03-29 10:06:34 +0300

    feat(auth): add JWT support

--- backend/auth-service/src/main/java/com/cherkizon/auth/config/SecurityConfig.java
+++ backend/auth-service/src/main/java/com/cherkizon/auth/config/SecurityConfig.java
@@ -1,4 +1,58 @@
 package com.cherkizon.auth.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    private final JwtAuthFilter jwtAuthFilter;
+
+    public SecurityConfig(JwtAuthFilter jwtAuthFilter) {
+        this.jwtAuthFilter = jwtAuthFilter;
+    }
+
+    private static final String[] AUTH_WHITELIST = {
+            "/swagger-resources/**",
+            "/swagger-ui.html",
+            "/v3/api-docs",
+            "/webjars/**",
+            "/api/auth/login",
+            "/api/auth/register"
+    };
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.disable())
+                .authorizeHttpRequests((authz) ->
+                        authz
+                                .requestMatchers(AUTH_WHITELIST).permitAll()
+                            //    .requestMatchers("/ads/**", "/users/**").authenticated()         //
+                )
+
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .httpBasic(withDefaults());
+
+        return http.build();
+    }
 }
--- backend/auth-service/src/main/java/com/cherkizon/auth/repository/TokenRepository.java
+++ backend/auth-service/src/main/java/com/cherkizon/auth/repository/TokenRepository.java
@@ -1,4 +1,25 @@
 package com.cherkizon.auth.repository;
 
-public interface TokenRepository {
+import com.cherkizon.auth.entity.Token;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
+import java.util.Optional;
+
+public interface TokenRepository extends JpaRepository<Token, Long> {
+
+    @Query("""
+        select t from Token t
+        where t.user.id = :userId
+        and t.revoked = false
+        and t.expiryDate > current_timestamp
+    """)
+    List<Token> findAllValidTokensByUser(Long userId);
+    Optional<Token> findByToken(String token);
+
+    boolean existsByToken(String token);
+    @Modifying
+    @Query("update Token t set t.revoked = true where t.user.id = :userId")
+    void revokeAllUserTokens(Long userId);
 }
