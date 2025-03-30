fedcd2e1cb55a4472e2c1769faab15c5334ac8be
denis <denistigrov@icloud.com>
2025-03-30 23:30:53 +0300

    chore: create tests class

---
+++ backend/ads-service/src/test/java/ru/relex/AbsServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package ru.relex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AbsServiceApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
