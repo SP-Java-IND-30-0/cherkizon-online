e755871a1d024cffbecfae13fb342708ec72546c
Belousoveu <belousov.eu@gmail.com>
2025-04-02 22:49:02 +0300

    feat (notification-service): Configure async email sending Implement retry policy for failed email deliveries Implement asynchronous email delivery service

---
+++ backend/notification-service/src/main/java/com/github/spjavaind300/notificationservice/config/RetryConfig.java
@@ -0,0 +1,27 @@
+package com.github.spjavaind300.notificationservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.retry.backoff.FixedBackOffPolicy;
+import org.springframework.retry.policy.SimpleRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
+
+@Configuration
+@EnableRetry
+public class RetryConfig {
+
+    @Bean
+    public RetryTemplate retryTemplate() {
+        RetryTemplate retryTemplate = new RetryTemplate();
+        FixedBackOffPolicy fixedBackOffPolicy = new FixedBackOffPolicy();
+        fixedBackOffPolicy.setBackOffPeriod(2000);
+
+        SimpleRetryPolicy retryPolicy = new SimpleRetryPolicy();
+        retryPolicy.setMaxAttempts(5);
+
+        retryTemplate.setBackOffPolicy(fixedBackOffPolicy);
+        retryTemplate.setRetryPolicy(retryPolicy);
+        return retryTemplate;
+    }
+}
