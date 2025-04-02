14a8d01813357ea6acbdbafa5cc3d1f83f6b09ab
denis <denistigrov@icloud.com>
2025-04-03 00:37:20 +0300

    chore: create pom.xml/ application.properties / application.yml

--- backend/ads-service/src/main/java/ru/relex/AdsServiceApplication.java
+++ backend/ads-service/src/main/java/ru/relex/AdsServiceApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AbsServiceApplication {
+public class AdsServiceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(AbsServiceApplication.class, args);
+        SpringApplication.run(AdsServiceApplication.class, args);
     }
 }
