ff1f886cdd1dba766a2e184374390bf32bc43270
Belousoveu <belousov.eu@gmail.com>
2025-04-06 03:03:02 +0300

    test (notification-service): Add tests for new functionality

---
+++ backend/notification-service/src/test/java/com/github/spjavaind300/notificationservice/service/imp/AdvServiceImpTest.java
@@ -0,0 +1,39 @@
+package com.github.spjavaind300.notificationservice.service.imp;
+
+import com.github.spjavaind300.notificationservice.client.AdvServiceClient;
+import com.github.spjavaind300.notificationservice.model.dto.AdvDto;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class AdvServiceImpTest {
+
+    @Mock
+    private AdvServiceClient client;
+
+    private AdvServiceImp advServiceImp;
+
+    @BeforeEach
+    void setUp() {
+        advServiceImp = new AdvServiceImp(client);
+    }
+
+    @Test
+    void getAdv() {
+
+        AdvDto advDto = new AdvDto(1, "adv_title");
+        when(client.getAdv(1)).thenReturn(advDto);
+
+        AdvDto result = advServiceImp.getAdv(1);
+
+        assertNotNull(result);
+        assertEquals(advDto, result);
+
+    }
+}
---
+++ backend/notification-service/src/test/java/com/github/spjavaind300/notificationservice/service/imp/ProfileServiceImpTest.java
@@ -0,0 +1,46 @@
+package com.github.spjavaind300.notificationservice.service.imp;
+
+import com.github.spjavaind300.notificationservice.client.ProfileServiceClient;
+import com.github.spjavaind300.notificationservice.model.dto.UserDto;
+import com.github.spjavaind300.notificationservice.service.ProfileService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class ProfileServiceImpTest {
+
+    @Mock
+    private ProfileServiceClient profileServiceClient;
+
+    private ProfileService profileService;
+
+    @BeforeEach
+    void setUp() {
+        profileService = new ProfileServiceImp(profileServiceClient);
+    }
+
+    @Test
+    void test_getProfiles() {
+
+        UserDto user1 = new UserDto(1,"user1_firstName", "user1_lastName", "user1@test.com");
+        UserDto user2 = new UserDto(2,"user2_firstName", "user2_lastName", "user2@test.com");
+
+        when(profileServiceClient.getProfiles(List.of(1,2))).thenReturn(Set.of(user1,user2));
+
+        List<UserDto> users = profileService.getProfiles(List.of(1,2));
+
+        assertNotNull(users);
+        assertEquals(2, users.size());
+        assertTrue(users.contains(user1));
+        assertTrue(users.contains(user2));
+    }
+}
