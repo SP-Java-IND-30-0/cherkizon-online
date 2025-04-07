e40fdbaa0b824b6e9554979634483232e02f5a55
Unique-Kris <titina91@mail.ru>
2025-04-07 09:02:25 +0300

    delete logger in FeignConfig

--- backend/comments-service/src/main/java/com/github/spjavaind300/commentsservice/config/FeignConfig.java
+++ backend/comments-service/src/main/java/com/github/spjavaind300/commentsservice/config/FeignConfig.java
@@ -1,17 +1,11 @@
 package com.github.spjavaind300.commentsservice.config;
 
-import feign.Logger;
 import feign.Retryer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class FeignConfig {
-
-    @Bean
-    public Logger.Level feignLoggerLevel() {
-        return Logger.Level.BASIC;
-    }
 
     @Bean
     public Retryer feignRetryer() {
