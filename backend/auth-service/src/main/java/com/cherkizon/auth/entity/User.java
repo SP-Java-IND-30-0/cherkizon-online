1aaab95eeb34b2373f8535c7ec71cf2acc8d9be1
Goodwin999 <Vyacheslav.tsitsar@mail.ru>
2025-04-07 14:22:55 +0300

    refactor (auth): corrected the annotation

--- backend/auth-service/src/main/java/com/cherkizon/auth/entity/User.java
+++ backend/auth-service/src/main/java/com/cherkizon/auth/entity/User.java
@@ -14,6 +14,7 @@ import org.springframework.security.core.GrantedAuthority;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private Long id;
 
     @Column(unique = true, nullable = false, length = 32)
