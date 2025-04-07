2db41514a9268d819c29d45435877708b07ebba7
Belousoveu <belousov.eu@gmail.com>
2025-04-07 15:22:00 +0300

    feat (profile-service): Setup project template Configure database connection Implement database migrations Create entity class

---
+++ backend/profile-service/init.sql
@@ -0,0 +1,16 @@
+DROP DATABASE IF EXISTS profile_service_db;
+DROP ROLE IF EXISTS dev;
+CREATE USER dev WITH PASSWORD '1';
+
+CREATE DATABASE profile_service_db;
+GRANT ALL PRIVILEGES ON DATABASE profile_service_db TO dev;
+
+\c profile_service_db
+
+CREATE SCHEMA IF NOT EXISTS app;
+GRANT ALL PRIVILEGES ON SCHEMA app TO dev;
+
+CREATE SCHEMA IF NOT EXISTS service;
+GRANT ALL PRIVILEGES ON SCHEMA service TO dev;
+
+ALTER ROLE dev SET search_path TO app, public;
---
+++ backend/profile-service/src/main/java/com/github/spjavaind300/profileservice/model/entity/User.java
@@ -0,0 +1,41 @@
+package com.github.spjavaind300.profileservice.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+
+
+@Entity
+@Table(name = "users", schema = "app")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class User {
+
+    @Id
+    @Column(name = "id", nullable = false, unique = true)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "image")
+    private String image;
+
+
+}
