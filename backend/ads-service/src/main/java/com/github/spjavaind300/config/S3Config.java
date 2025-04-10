bbe1048028f3caf7bf391b54c9d48dacbe1b0b63
Belousoveu <belousov.eu@gmail.com>
2025-04-10 19:59:09 +0300

    chore (ads-service): Update folder structure to match architecture

--- backend/ads-service/src/main/java/com/github/spjavaind300/config/S3Config.java
+++ backend/ads-service/src/main/java/com/github/spjavaind300/config/S3Config.java
@@ -1,4 +1,4 @@
-package ru.relex.config;
+package com.github.spjavaind300.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
--- backend/ads-service/src/main/java/com/github/spjavaind300/model/entity/Ad.java
+++ backend/ads-service/src/main/java/com/github/spjavaind300/model/entity/Ad.java
@@ -1,4 +1,4 @@
-package ru.relex.model.entity;
+package com.github.spjavaind300.model.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
