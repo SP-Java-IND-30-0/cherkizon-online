20f6b78633e7bc4b2d5cca59e9df9caae221e0f2
Belousoveu <belousov.eu@gmail.com>
2025-04-14 16:12:20 +0300

    fix (ads-service): add `@Repository` annotation

--- backend/ads-service/src/main/java/com/github/spjavaind300/repository/OutboxEventRepository.java
+++ backend/ads-service/src/main/java/com/github/spjavaind300/repository/OutboxEventRepository.java
@@ -2,7 +2,9 @@ package com.github.spjavaind300.repository;
 
 import com.github.spjavaind300.model.entity.OutboxEvent;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OutboxEventRepository extends JpaRepository<OutboxEvent, Long> {
     Iterable<OutboxEvent> findAllByProcessedIsFalse();
 }
