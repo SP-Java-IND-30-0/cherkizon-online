fbfba9c2bb07cc5a2a33b50c1ede72e1d20e22dd
art-idea5642 <chertezhy.ru@gmail.com>
2025-04-15 22:22:42 +0200

    feat(profile): added service module

---
+++ backend/profile-service/src/main/java/com/github/spjavaind300/profileservice/service/ProfileService.java
@@ -0,0 +1,17 @@
+package com.github.spjavaind300.profileservice.service;
+
+import com.github.spjavaind300.profileservice.dto.UpdateUserDTO;
+import com.github.spjavaind300.profileservice.dto.UserDTO;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+public interface ProfileService {
+    UserDTO getProfile(Long userId);
+
+    UpdateUserDTO updateProfile(Long userId, UpdateUserDTO updatedData);
+
+    void deleteProfile (Long targetUserId, String token);
+
+    String saveOrUpdateAvatar(Long userId, MultipartFile file) throws IOException;
+}
