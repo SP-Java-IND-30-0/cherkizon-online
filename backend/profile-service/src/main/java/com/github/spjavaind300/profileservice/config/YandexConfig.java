47f0b0a6c3c0d0f015d59bdab29fe5baaf63b4b6
art-idea5642 <chertezhy.ru@gmail.com>
2025-04-15 22:28:26 +0200

    feat(profile): added service module

---
+++ backend/profile-service/src/main/java/com/github/spjavaind300/profileservice/config/YandexConfig.java
@@ -0,0 +1,39 @@
+package com.github.spjavaind300.profileservice.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import lombok.Data;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.core.client.config.ClientOverrideConfiguration;
+import software.amazon.awssdk.http.apache.ApacheHttpClient;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+import java.net.URI;
+
+@Configuration
+@ConfigurationProperties(prefix = "aws.s3")
+@Data
+public class YandexConfig {
+
+    private String bucketName;
+    private String accessKey;
+    private String secretKey;
+    private String region;
+    private String endpoint;
+
+    @Bean
+    public S3Client s3Client() {
+        AwsCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
+        return S3Client.builder()
+                .httpClient(ApacheHttpClient.create())
+                .region(Region.of(region))
+                .endpointOverride(URI.create(endpoint))
+                .credentialsProvider(StaticCredentialsProvider.create(credentials))
+                .overrideConfiguration(ClientOverrideConfiguration.builder().build())
+                .build();
+    }
+}
