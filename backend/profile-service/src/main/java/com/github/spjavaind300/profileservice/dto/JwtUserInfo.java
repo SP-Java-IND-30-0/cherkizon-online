c7f2c2468feb414ae7b11ddba577703641dde859
art-idea5642 <chertezhy.ru@gmail.com>
2025-04-15 22:08:05 +0200

    feat(profile): added service module

---
+++ backend/profile-service/src/main/java/com/github/spjavaind300/profileservice/dto/JwtUserInfo.java
@@ -0,0 +1,14 @@
+package com.github.spjavaind300.profileservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class JwtUserInfo {
+    private long userId;
+    private String role;
+
+}
