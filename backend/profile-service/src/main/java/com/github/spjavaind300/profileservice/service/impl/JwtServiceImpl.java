c4e6918ffb7b26d194ac35d5f6562076c447fc59
art-idea5642 <chertezhy.ru@gmail.com>
2025-04-15 22:29:27 +0200

    feat(profile): added service module

--- backend/profile-service/src/main/java/com/github/spjavaind300/profileservice/service/impl/JwtServiceImpl.java
+++ backend/profile-service/src/main/java/com/github/spjavaind300/profileservice/service/impl/JwtServiceImpl.java
@@ -1,4 +1,14 @@
 package com.github.spjavaind300.profileservice.service.impl;
 
-public class JwtServiceImpl {
+import com.github.spjavaind300.profileservice.dto.JwtUserInfo;
+import com.github.spjavaind300.profileservice.service.JwtService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtServiceImpl implements JwtService {
+
+    @Override
+    public JwtUserInfo parseToken(String token) {
+        return new JwtUserInfo(1L, "USER");
+    }
 }
