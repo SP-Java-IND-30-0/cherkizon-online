7b1b168439e5aec0611652e791181a318df3ff67
Unique-Kris <titina91@mail.ru>
2025-04-17 20:46:56 +0300

    feat (comments-service): fix

--- backend/comments-service/src/main/java/com/github/spjavaind300/commentsservice/config/SecurityConfig.java
+++ backend/comments-service/src/main/java/com/github/spjavaind300/commentsservice/config/SecurityConfig.java
@@ -2,9 +2,11 @@ package com.github.spjavaind300.commentsservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @Configuration
 public class SecurityConfig {
@@ -20,7 +22,10 @@ public class SecurityConfig {
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .csrf(csrf -> csrf
-                        .ignoringRequestMatchers("/internal/**", "/api/**")
+                        .ignoringRequestMatchers("/internal/**", "/ads/**")
+                )
+                .exceptionHandling(ex -> ex
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
                 );
 
         return http.build();
