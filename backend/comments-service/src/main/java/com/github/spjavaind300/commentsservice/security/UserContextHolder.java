fd0422f42b84b037802fd18db40b198b0f23d40b
Unique-Kris <titina91@mail.ru>
2025-04-20 12:46:18 +0300

    feat (comments-service): fix

--- backend/comments-service/src/main/java/com/github/spjavaind300/commentsservice/security/UserContextHolder.java
+++ backend/comments-service/src/main/java/com/github/spjavaind300/commentsservice/security/UserContextHolder.java
@@ -3,7 +3,9 @@ package com.github.spjavaind300.commentsservice.security;
 import com.github.spjavaind300.commentsservice.dto.UserContext;
 
 public class UserContextHolder {
+
     private static final ThreadLocal<UserContext> contextHolder = new ThreadLocal<>();
+    private static final ThreadLocal<String> tokenHolder = new ThreadLocal<>();
 
     public static void setContext(UserContext context) {
         contextHolder.set(context);
@@ -13,7 +15,16 @@ public class UserContextHolder {
         return contextHolder.get();
     }
 
+    public static void setToken(String token) {
+        tokenHolder.set(token);
+    }
+
+    public static String getToken() {
+        return tokenHolder.get();
+    }
+
     public static void clear() {
         contextHolder.remove();
+        tokenHolder.remove();
     }
 }
