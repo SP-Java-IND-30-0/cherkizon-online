128f39b4c4221e34ce8be657275461110038ffe6
art-idea5642 <chertezhy.ru@gmail.com>
2025-04-20 15:12:05 +0200

    fix(profile): fix-image-endpoint

--- backend/profile-service/src/main/java/com/github/spjavaind300/profileservice/exception/AvatarNotFoundException.java
+++ backend/profile-service/src/main/java/com/github/spjavaind300/profileservice/exception/AvatarNotFoundException.java
@@ -4,4 +4,7 @@ public class AvatarNotFoundException extends RuntimeException {
     public AvatarNotFoundException(String key, Throwable cause) {
         super("Avatar not found: " + key, cause);
     }
+    public AvatarNotFoundException(String message) {
+        super(message);
+    }
 }
